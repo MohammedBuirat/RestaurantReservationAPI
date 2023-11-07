7ab1b56aa151130e545698228604c5c18f9586c8
Mohammed Buirat <1192896@student.birzeit.edu>
2023-11-07 12:11:10 +0200

    Implementing Controllers

---
+++ RestaurantReservationAPI/RestaurantReservationAPI/Controllers/MenuItemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantReservation.Controllers
+{
+    public class MenuItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
